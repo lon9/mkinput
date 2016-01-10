76c947fb6f9f2670f730dece02f4d1fb54739b26
Rompei <rompei0924@gmail.com>
2016-01-10 13:30:17 +0900

    Change output to stdout.

--- main.go
+++ main.go
@@ -2,17 +2,10 @@ package main
 
 import (
 	"encoding/json"
-	"flag"
 	"os"
 )
 
 func main() {
-
-	var filepath = flag.String("f", "input.txt", "filepath for generated file")
-	flag.Parse()
-	if filepath == nil {
-		*filepath = "input.txt"
-	}
 
 	// Reading Json rom stdin.
 	dec := json.NewDecoder(os.Stdin)
@@ -21,8 +14,5 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	err = g.Generate(*filepath)
-	if err != nil {
-		panic(err)
-	}
+	g.Generate()
 }
